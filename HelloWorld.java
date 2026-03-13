d4fec95ec1adae8d74bd5b44e0308b8556c493e8
Saakshi-c-shetty <shettysaakshic@gmail.com>
2026-03-13 09:36:10 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,9 @@
 public class HelloWorld{
   public static void main(String [] args){
     System.out.println("hello all ");
+    int a=12;
+    int b=10;
+    int sum =a+b;
+    System.out.println("sum is : "+sum);
   }
 }
