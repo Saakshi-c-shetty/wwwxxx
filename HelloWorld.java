80ea54590bc09324db54eb7d5f56f520a4e08eb7
Saakshi-c-shetty <shettysaakshic@gmail.com>
2026-03-13 09:38:11 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,15 @@
 public class HelloWorld{
   public static void main(String [] args){
-    System.out.println("hello all ");
-    int a=12;
-    int b=10;
-    int sum =a+b;
-    System.out.println("sum is : "+sum);
-    System.out.println("sum is : "+sum);
-  }
+    int a = 5;
+        int b = 10;
+
+        System.out.println("Before swapping:");
+        System.out.println("a = " + a + ", b = " + b);
+
+        int temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println("After swapping:");
+        System.out.println("a = " + a + ", b = " + b);
 }
